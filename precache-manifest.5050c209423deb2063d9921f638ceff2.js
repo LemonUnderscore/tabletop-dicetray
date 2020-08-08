823fdab2cbb10336be59ea34f3d38a8ae2f98676
Conor Clerkin <38741780+LemonUnderscore@users.noreply.github.com>
2020-08-08 17:40:58 +0100

    Updates

---
+++ precache-manifest.5050c209423deb2063d9921f638ceff2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c62ddbd7de9961d467dfb737dac30693",
+    "url": "/tabletop-dicetray/index.html"
+  },
+  {
+    "revision": "8c1e2359d08e53a85877",
+    "url": "/tabletop-dicetray/static/css/main.e5cab88c.chunk.css"
+  },
+  {
+    "revision": "b616f10f49899ff38d0a",
+    "url": "/tabletop-dicetray/static/js/2.5cf3df1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tabletop-dicetray/static/js/2.5cf3df1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c1e2359d08e53a85877",
+    "url": "/tabletop-dicetray/static/js/main.2136d867.chunk.js"
+  },
+  {
+    "revision": "ef3159b2263624945072",
+    "url": "/tabletop-dicetray/static/js/runtime-main.712f1a59.js"
+  },
+  {
+    "revision": "74d1e9877b996b1b561be7bc0edae75e",
+    "url": "/tabletop-dicetray/static/media/d10icon.74d1e987.png"
+  },
+  {
+    "revision": "bbf157a8b14545668f7a4fd0ae9debc6",
+    "url": "/tabletop-dicetray/static/media/d12icon.bbf157a8.png"
+  },
+  {
+    "revision": "bdc5c8fe6ab697a72589dd6ddb5cca49",
+    "url": "/tabletop-dicetray/static/media/d20icon.bdc5c8fe.png"
+  },
+  {
+    "revision": "cd39c77504b9f7319d723c224a6941db",
+    "url": "/tabletop-dicetray/static/media/d8icon.cd39c775.png"
+  },
+  {
+    "revision": "213a9fb4f6352c87b051ae5d3948b25d",
+    "url": "/tabletop-dicetray/static/media/delete.213a9fb4.png"
+  },
+  {
+    "revision": "7042f3f36d7b1406b0d8d41c4435f25f",
+    "url": "/tabletop-dicetray/static/media/reRoll.7042f3f3.png"
+  }
+]);
